19ac2c68b22cdfba65a6ecd76e7316149ef3d6f7
LKWLaLa <lkwlala@gmail.com>
2018-03-18 20:40:17 -0400

    refactor into functions

--- app/app.rb
+++ app/app.rb
@@ -1,32 +1,41 @@
 require 'redis'
 require 'rest-client'
 
-redis = Redis.new(url: 'redis://redis:6379')
-
-collections = redis.keys.map do |k| 
-  if redis.type(k) == "list"
-    redis.lrange(k, 0, -1)
-  elsif redis.type(k) == "set"
-    redis.smembers(k) 
-  else
-    next
+def collect_values_from_redis
+  redis = Redis.new(url: 'redis://redis:6379')
+  redis.keys.map do |k| 
+    if redis.type(k) == "list"
+      redis.lrange(k, 0, -1)
+    elsif redis.type(k) == "set"
+      redis.smembers(k) 
+    else
+      next
+    end
   end
 end
 
 def no_anagrams?(collection)
-  sorted_collection = collection.map {|item| item.chars.sort.join}
+  sorted_collection = collection.map {|num_str| num_str.chars.sort.join}
   sorted_collection.uniq.length == collection.length
 end
 
-anagram_free_collections = collections.select {|c| no_anagrams?(c)}
-
-integer_collections = anagram_free_collections.map do |c|
-  c.map{|num_string| num_string.to_i }
+def anagram_free_collections
+  collect_values_from_redis().select {|c| no_anagrams?(c)}
 end
 
-differences = integer_collections.map{|c| c.max - c.min}
+def differences_collection
+  int_collections = anagram_free_collections().map do |c|
+    c.map{|num_str| num_str.to_i}
+  end
+  int_collections.map{|c| c.max - c.min}
+end
 
-checksum = differences.reduce(:+)   
+def send_checksum
+  checksum = differences_collection().reduce(:+) 
+  puts "The checksum is #{checksum}."  
+  puts RestClient.get("http://answer:3000/#{checksum}")
+end
 
-puts RestClient.get("http://answer:3000/#{checksum}")
+send_checksum()
+
 
